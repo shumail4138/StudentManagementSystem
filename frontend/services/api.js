9985da205366572dd586c79f64fd1b6d82ffe14a
Shumail <shumailrahatkhan@gmail.com>
2026-07-19 16:06:29 +0530

    Fix API base URL

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,19 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "https://studentmanagementsystem-46mg.onrender.com",
+  baseURL: "https://studentmanagementsystem-46mg.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
-  if(token){
+  if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
-
 });
 
 export default api;
